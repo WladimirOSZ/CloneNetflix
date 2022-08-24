d64fadfb6140780241665f0deef7678537f600f0
WLADIMIR <cg3014428@LAB204M37.IFSP>
2022-08-23 21:59:50 -0300

    Trazendo dados da api do TMDB

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(()=> {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+      setMovieList(list);
+      
+      let originals = list.filter(i=>i.slug === 'originals');//lambda
+      let randomChosen = Math.floor(Math.random()* (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id,'tv');
+      console.log(chosenInfo);
+      setFeaturedData(chosenInfo);
+    };
+    loadAll();
+  }, []);
+  
+
+  return (
+    <div className='page'>
+
+    </div>
+  );
+}
+
