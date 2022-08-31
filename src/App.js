27b14f24f31286b61881520ef62cac088ea7d8ef
WLADIMIR <cg3014428@LAB204M38.IFSP>
2022-08-30 22:18:29 -0300

    adicionada as listas de filmes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Tmdb from './Tmdb'
 import Header from './components/Header'
 import FeaturedMovie from './components/Featured/'
+import MovieRow from './components/MovieRow'
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
@@ -24,6 +25,23 @@ export default () => {
     loadAll();
   }, []);
   
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY>10){
+        setBlackHeader(true)
+      }else{
+        setBlackHeader(false)
+      }
+      window.addEventListener('scroll', scrollListener)
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+
+  },[])
 
   return (
     <div className="page">
@@ -32,6 +50,25 @@ export default () => {
         {featuredData && 
           <FeaturedMovie item={featuredData}/>
         }
+
+        <div className='lists'>
+          {movieList.map((item,key)=>
+            <MovieRow key={key} title={item.title} items={item.items} />
+          )}
+        </div>
+
+        <footer>
+          Feito por Wladimir Oliveira Souza<br/>
+          Direitos de imagem para Netflix
+        </footer>
+
+        {movieList.length <=0 &&
+         <div className='loading'>
+           <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt='Carregando'/>
+         </div>
+        
+        }
+
     </div>
   );
 }
